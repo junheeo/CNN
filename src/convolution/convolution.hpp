5d84a90924ef50a0dc81fd277ac15858b641e270
Junhee Oh <junheeoh@Junhees-MacBook-Pro.local>
2025-02-08 13:20:31 +0900

    first commit

---
+++ src/convolution/convolution.hpp
@@ -0,0 +1,225 @@
+#include <iostream>
+
+
+struct dim3_t {
+    size_t d;
+    size_t w;
+    size_t h;
+};
+
+template<size_t D, size_t W, size_t H>
+class Xmatrix;
+
+template<size_t D_prev, size_t D_curr, size_t W, size_t H>
+class Wmatrix;
+
+template<size_t D, size_t W, size_t H>
+class Ymatrix{
+    friend class Xmatrix<D,W,H>;
+    double (*ptrarr)[D][W][H];
+    public:
+    dim3_t range;
+    Ymatrix(double (*ptr_array)[D][W][H]) : ptrarr(ptr_array) 
+        {range = {D,W,H};}
+    double operator() (size_t z, size_t x, size_t y){
+        if(z>=0 && x>=0 && y>=0 && z<D && x<W && y<H){
+            return (*ptrarr)[z][x][y];
+        } else {
+            std::cout<<"Ymatrix index "<<z<<","<<x<<","<<y<<" out of range"<<std::endl;
+            throw 0;
+        }
+    }
+    double operator() (dim3_t position){
+        return (*this)(position.d, position.w, position.h);
+    }
+    void setVal(size_t z, size_t x, size_t y, double val){
+        (*ptrarr)[z][x][y]=val;
+    }
+    void setVal(dim3_t inx, double val){
+        (*ptrarr)[inx.d][inx.w][inx.h]=val;
+    }
+
+    dim3_t& dim() {return range;}
+    void printMatrixForm(){
+        for(int z=0;z<range.d;++z){
+            std::cout<<"depth "<<z<<std::endl;
+        for(int x=0;x<range.w;++x){
+            std::cout<<"    ";
+        for(int y=0;y<range.h;++y){
+            std::cout<<(*this)(z,x,y)<<" ";
+        }
+            std::cout<<std::endl;
+        }
+            std::cout<<std::endl;
+        }
+    }
+};
+
+template<size_t D_y, size_t W_y, size_t H_y>
+class Xmatrix{
+    Ymatrix<D_y,W_y,H_y> whole;
+    public:
+    dim3_t windowSize;
+    dim3_t beginInx;
+    Xmatrix(Ymatrix<D_y,W_y,H_y> Y, size_t D_x, size_t W_x, size_t H_x) : whole(Y) {
+        windowSize = {D_x, W_x, H_x};
+        beginInx = {0, 0, 0};
+    }
+    void setStart(size_t z, size_t x, size_t y) {
+        if( z < whole.range.d-beginInx.d+1 && 
+            x < whole.range.w-beginInx.w+1 &&
+            y < whole.range.h-beginInx.h+1
+          ) {
+            beginInx = {z, x, y};
+        } else {
+            std::cout<<"Xmatrix::setStart "<<z<<" "<<x<<" "<<y<<" out of range"<<std::endl;
+            throw 0;
+        }
+    }
+    double operator()(size_t z, size_t x, size_t y){
+        if(z<windowSize.d && x<windowSize.w && y<windowSize.h){
+            size_t whole_z = beginInx.d + z;
+            size_t whole_x = beginInx.w + x;
+            size_t whole_y = beginInx.h + y;
+            return whole(whole_z, whole_x, whole_y);
+        } else {
+            std::cout<<"Xmatrix "<<z<<","<<x<<","<<y<<" index out of range"<<std::endl;
+            throw 0;
+        }
+    }
+    double operator()(dim3_t position){
+        return (*this)(position.d, position.w, position.h);
+    }
+    void setVal(size_t z, size_t x, size_t y, double val){
+        if(z<windowSize.d && x<windowSize.w && y<windowSize.h){
+            size_t whole_z = beginInx.d + z;
+            size_t whole_x = beginInx.w + x;
+            size_t whole_y = beginInx.h + y;
+            whole.setVal(whole_z, whole_x, whole_y, val);
+        } else {
+            std::cout<<"Xmatrix "<<z<<","<<x<<","<<y<<" index out of range"<<std::endl;
+            throw 0;
+        }
+    }
+    void setVal(dim3_t inx, double val){
+        setVal(inx.d, inx.w, inx.h, val);
+    }
+
+};
+
+
+
+template<size_t D_prev, size_t D_curr, size_t W, size_t H>
+class Wmatrix{
+    double (*ptrarr)[D_prev][D_curr][W][H];
+    public:
+    dim3_t colsDim;
+    size_t rowDim;
+        Wmatrix(double (*ptr_array)[D_prev][D_curr][W][H])
+                : ptrarr(ptr_array) {
+                    colsDim = {D_prev, W, H};
+                    rowDim = D_curr;
+                }
+        double operator() (size_t z_prev, size_t z_curr, size_t x, size_t y){
+            if( z_prev<colsDim.d &&
+                z_curr<rowDim &&
+                x<colsDim.w &&
+                y<colsDim.h){
+                return (*ptrarr)[z_prev][z_curr][x][y];
+            }else{
+                std::cout<<"Wmatrix index "<<z_prev<<" "<<z_curr<<" "<<x<<" "<<y<<" out of range"<<std::endl;
+                throw 0;
+            }
+        }
+        double operator() (size_t rowInx, dim3_t colInx){
+            return (*this)(colInx.d, rowInx, colInx.w, colInx.h);
+        }
+        double transpose (dim3_t rowInx, size_t colInx){
+            return (*this)(rowInx.d, colInx, rowInx.w, rowInx.h);
+        }
+
+        void printMatrixForm(){
+            for(size_t rowInx=0;rowInx<rowDim;++rowInx){
+            std::cout<<"row "<<rowInx<<std::endl;
+            for(size_t z=0;z<colsDim.d;++z){
+            for(size_t x=0;x<colsDim.w;++x){
+                std::cout<<"    ";
+            for(size_t y=0;y<colsDim.h;++y){
+                dim3_t colInx = {z,x,y};
+                std::cout<<(*this)(rowInx, colInx)<<" ";
+            }
+                std::cout<<std::endl;
+            }
+                std::cout<<std::endl;
+            }
+                std::cout<<std::endl;
+            }
+
+        }
+        void printMatrixTransposeForm(){
+            for(size_t z=0;z<colsDim.d;++z){
+            for(size_t x=0;x<colsDim.w;++x){
+            for(size_t y=0;y<colsDim.h;++y){
+                dim3_t rowInx = {z,x,y};
+                for(size_t colInx=0;colInx<rowDim;++colInx)
+                {
+                    std::cout<<"(col"<<colInx<<")"<<(*this).transpose(rowInx, colInx)<<" ";
+                }
+                std::cout<<std::endl;
+            }
+                std::cout<<std::endl;
+            }
+                std::cout<<std::endl;
+            }
+        }
+};
+
+
+
+template<size_t D_prev, size_t D_curr, size_t W_window, size_t H_window, size_t W_prev, size_t H_prev, size_t W_curr, size_t H_curr>
+void matMult(Wmatrix<D_prev,D_curr,W_window,H_window> W,
+            Xmatrix<D_prev,W_prev,H_prev> X,
+            Ymatrix<D_curr,W_curr,H_curr> Y){
+    /*calculate Y for Y=WX*/
+    if(X.windowSize.d != D_prev){
+        std::cout<<"window Xl_0 should cover the entire depth"<<std::endl;
+        throw 2;
+    }
+    if(X.windowSize.w != W_window ||
+       X.windowSize.h != H_window){
+        std::cout<<"window size provided to matMult template is not same ase window size in Wmatrix parameter"<<std::endl;
+        throw 2;
+    }
+    
+    dim3_t Y_inx;
+    double Y_inx_Val;
+    size_t W_row_inx;
+    dim3_t W_col_inx;
+    dim3_t X_col_inx;
+    /* x_curr and y_curr is position on output Y_l
+     * and also position of window X_l on Y_l-1
+     */
+    for(size_t z_curr=0;z_curr<D_curr;++z_curr){
+    for(size_t x_curr=0;x_curr<W_curr;++x_curr){
+    for(size_t y_curr=0;y_curr<H_curr;++y_curr){
+        Y_inx = {z_curr,x_curr,y_curr};
+        X.setStart(0,x_curr,y_curr);
+
+        Y_inx_Val=0.0;
+        for(size_t z_prev=0;z_prev<D_prev;++z_prev){
+        for(size_t x_prev=0;x_prev<W_window;++x_prev){
+        for(size_t y_prev=0;y_prev<H_window;++y_prev){
+            W_row_inx = z_curr;
+            W_col_inx = {z_prev,x_prev,y_prev};
+            X_col_inx = {z_prev,x_prev,y_prev};
+
+            Y_inx_Val += W(W_row_inx,W_col_inx)*X(X_col_inx);
+        }
+        }
+        }
+        Y.setVal(Y_inx, Y_inx_Val);
+    }
+    }
+    }
+    
+}
---
+++ src/test/main.cpp
@@ -0,0 +1,273 @@
+#include<iostream>
+#include"convolution.hpp"
+
+void print_image(){
+    std::cout<<"double image[2][3][4] = {\n"
+"    {\n"
+"        {1.2, 1.3, 1.4, 1.5}, \n"
+"        {1.6, 1.7, 1.8, 1.9}, \n"
+"        {2.0, 2.1, 2.2, 2.3}\n"
+"    }, \n"
+"    {\n"
+"        {2.4, 2.5, 2.6, 2.7}, \n"
+"        {2.8, 2.9, 3.0, 3.1}, \n"
+"        {3.2, 3.3, 3.4, 3.5}\n"
+"    }\n"
+"};"<<std::endl;
+}
+
+void testYandXmatrix(){
+    std::cout<<"testYandXmatrix()*-*-*-*-"<<std::endl;
+    print_image();
+    double image[2][3][4] = {
+    {
+        {1.2, 1.3, 1.4, 1.5}, 
+        {1.6, 1.7, 1.8, 1.9}, 
+        {2.0, 2.1, 2.2, 2.3}
+    }, 
+    {
+        {2.4, 2.5, 2.6, 2.7}, 
+        {2.8, 2.9, 3.0, 3.1}, 
+        {3.2, 3.3, 3.4, 3.5}
+    }
+    };
+    dim3_t image_dims = {2,3,4};
+    Ymatrix <2,3,4> Yl_0 (&image);
+    std::cout<<"Yl_0(1,2,3) must be 3.5 : "<<Yl_0(1,2,3)<<std::endl;
+    Yl_0.setVal(1,2,3,4.0);
+    std::cout<<"Yl_0(1,2,3) must be 4.0 : "<<Yl_0(1,2,3)<<std::endl;
+    Yl_0.setVal(1,2,3,3.5);
+    std::cout<<"Yl_0(1,2,3) must be 3.5 : "<<Yl_0(1,2,3)<<std::endl;
+
+
+    dim3_t& range_image = Yl_0.dim();
+    std::cout<<"Yl_0's dims are "<<range_image.d<<" "<<range_image.w<<" "<<range_image.h<<std::endl;
+
+    Xmatrix <2,3,4> Xl_0 (Yl_0, 2, 2, 3);
+    Xl_0.setStart(0,0,1);
+    std::cout<<"Xl_0 (Yl_0, 2, 2, 3) with Xl_0.setStart(0,0,1) : "<<std::endl;
+    for(int z=0;z<2;++z){
+    for(int x=0;x<2;++x){
+        std::cout<<"   ";
+    for(int y=0;y<3;++y){
+        std::cout<<Xl_0(z,x,y)<<" ";
+    }
+        std::cout<<std::endl;
+    }
+        std::cout<<std::endl;
+    }
+
+    Xl_0.setStart(0,1,1);
+    std::cout<<"Xl_0 (Yl_0, 2, 2, 3) with Xl_0.setStart(0,1,1) : "<<std::endl;
+    for(int z=0;z<2;++z){
+    for(int x=0;x<2;++x){
+        std::cout<<"   ";
+    for(int y=0;y<3;++y){
+        std::cout<<Xl_0(z,x,y)<<" ";
+    }
+        std::cout<<std::endl;
+    }
+        std::cout<<std::endl;
+    }
+
+    std::cout<<"Xl_0 (Yl_0, 2, 2, 3) with Xl_0.setStart(0,1,1) : "<<std::endl;
+    for(size_t z=0;z<2;++z){
+    for(size_t x=0;x<2;++x){
+        std::cout<<"   ";
+    for(size_t y=0;y<3;++y){
+        dim3_t col_position = {z,x,y};
+        std::cout<<Xl_0(col_position)<<" ";
+    }
+        std::cout<<std::endl;
+    }
+        std::cout<<std::endl;
+    }
+
+
+    std::cout<<"*-*-*-*-"<<std::endl<<std::endl;
+}
+
+void testWmatrix() {
+    double image[2][3][4] = {
+    {
+        {1.2, 1.3, 1.4, 1.5}, 
+        {1.6, 1.7, 1.8, 1.9}, 
+        {2.0, 2.1, 2.2, 2.3}
+    }, 
+    {
+        {2.4, 2.5, 2.6, 2.7}, 
+        {2.8, 2.9, 3.0, 3.1}, 
+        {3.2, 3.3, 3.4, 3.5}
+    }
+    };
+    dim3_t image_dims = {2,3,4};
+    Ymatrix <2,3,4> Yl_0 (&image);
+    Xmatrix <2,3,4> Xl_0 (Yl_0, 2, 2, 3);/*2,2,3 is window size*/
+    Xl_0.setStart(0,1,1);
+
+
+    double Weightarrl_1[2][4][2][3] = {
+        {
+            {
+                {5,6,7},
+                {8,9,10}
+            },
+            {
+                {17,18,19},
+                {20,21,22}
+            },
+            {
+                {29,30,31},
+                {32,33,34}
+            },
+            {
+                {41,42,43},
+                {44,45,46}
+            }
+        },
+        {
+            {
+                {11,12,13},
+                {14,15,16}
+            },
+            {
+                {23,24,25},
+                {26,27,28}
+            },
+            {
+                {35,36,37},
+                {38,39,40}
+            },
+            {
+                {47,48,49},
+                {50,51,52}
+            }
+        }
+    };
+
+    Wmatrix <2,4,2,3> Wl_1 (&Weightarrl_1);
+    /*
+    std::cout<<"Wl_1(p,q,r) = "<<std::endl;
+    for(size_t p=0;p<2;++p){
+    for(size_t q=0;q<4;++q){
+    for(size_t r=0;r<2;++r){
+        std::cout<<"    ";
+    for(size_t s=0;s<3;++s){
+        std::cout<<Wl_1(p,q,r,s)<<" ";
+    }
+        std::cout<<std::endl;
+    }
+        std::cout<<std::endl;
+    }
+        std::cout<<std::endl;
+    }
+    */
+    std::cout<<"The result should be in ascending order:"<<std::endl;
+    std::cout<<"Wl_1(size_T row, dim3_t col) = "<<std::endl;
+    Wl_1.printMatrixForm();
+    /*
+    for(size_t rowInx=0;rowInx<Wl_1.rowDim;++rowInx){
+        std::cout<<"row "<<rowInx<<std::endl;
+    for(size_t z=0;z<Wl_1.colsDim.d;++z){
+    for(size_t x=0;x<Wl_1.colsDim.w;++x){
+        std::cout<<"    ";
+    for(size_t y=0;y<Wl_1.colsDim.h;++y){
+        dim3_t colInx = {z,x,y};
+        std::cout<<Wl_1(rowInx, colInx)<<" ";
+    }
+        std::cout<<std::endl;
+    }
+        std::cout<<std::endl;
+    }
+        std::cout<<std::endl;
+    }
+    */
+    std::cout<<std::endl;
+    std::cout<<"Wl_1.transpose = "<<std::endl;
+    Wl_1.printMatrixTransposeForm();
+}
+
+void testWmatrixMultiply(){
+    double image[2][3][4] = {
+    {
+        {1.2, 1.3, 1.4, 1.5}, 
+        {1.6, 1.7, 1.8, 1.9}, 
+        {2.0, 2.1, 2.2, 2.3}
+    }, 
+    {
+        {2.4, 2.5, 2.6, 2.7}, 
+        {2.8, 2.9, 3.0, 3.1}, 
+        {3.2, 3.3, 3.4, 3.5}
+    }
+    };
+    dim3_t image_dims = {2,3,4};
+    Ymatrix <2,3,4> Yl_0 (&image);
+    /* window size of Xl_0 is 2,2,3 */
+    Xmatrix <2,3,4> Xl_0 (Yl_0, 2, 2, 3);
+    Xl_0.setStart(0,1,1);
+
+    double Weightarrl_1[2][4][2][3] = {
+        {
+            {
+                {5,6,7},
+                {8,9,10}
+            },
+            {
+                {17,18,19},
+                {20,21,22}
+            },
+            {
+                {29,30,31},
+                {32,33,34}
+            },
+            {
+                {41,42,43},
+                {44,45,46}
+            }
+        },
+        {
+            {
+                {11,12,13},
+                {14,15,16}
+            },
+            {
+                {23,24,25},
+                {26,27,28}
+            },
+            {
+                {35,36,37},
+                {38,39,40}
+            },
+            {
+                {47,48,49},
+                {50,51,52}
+            }
+        }
+    };
+
+    Wmatrix <2,4,2,3> Wl_1 (&Weightarrl_1);
+
+    /* size Yl_0 is 2,3,4
+     * window size of Xl_0 is 2,2,3
+     * size Wl_1 is 2,4,2,3 
+     * therefore
+     * size Yl_1 is 4, (3-2+1)=2, (4-3+1)=2 */
+    double Yl_1arr[4][2][2];
+    Ymatrix <4,2,2> Yl_1 (&Yl_1arr);
+
+    /* Xmatrix<D_prev=2, W_prev=3, H_prev=4> Xl_0
+     * Wmatrix<D_prev=2, D_curr=4, W_window=2, H_window=3> Wl_1
+     * Ymatrix<D_curr=4, W_curr=2, H_curr=2> Yl_1
+     * matMult<D_prev, D_curr, W_window, H_window, 
+     *         W_prev, H_prev, W_curr, H_curr> */
+    matMult<2,4,2,3,3,4,2,2>(Wl_1, Xl_0, Yl_1);
+    std::cout<<"Yl_1 = "<<std::endl;
+    Yl_1.printMatrixForm();
+}
+
+int main(){
+    /*testYandXmatrix();*/
+    /*testWmatrix();*/
+    testWmatrixMultiply();
+    return 0;
+}
